96eacd6a7e290a398e4d913443d924382c903b24
Mochi753 <179483962+mochi-753@users.noreply.github.com>
2026-06-16 21:32:31 +0900

    refactor(python): ""を''に変更

--- src/main/python/release.curseforge.py
+++ src/main/python/release.curseforge.py
@@ -8,36 +8,36 @@ import requests
 
 def main():
     metadata = {
-        "changelog": markdown.Markdown(extensions=['extra']).convert(Path("CHANGELOG.md").read_text(encoding="utf-8")),
-        "changelogType": "html",
-        "displayName": f"{os.environ.get("REPOSITORY_NAME")} {os.environ.get("VERSION")}",
-        "gameVersions": [11779],  # Minecraft 1.21.1
-        "gameVersionNames": ["Client", "Server", "NeoForge", "1.21.1"],
-        "releaseType": "release",
-        "relations": {
-            "projects": []
+        'changelog': markdown.Markdown(extensions=['extra']).convert(Path('CHANGELOG.md').read_text(encoding='utf-8')),
+        'changelogType': 'html',
+        'displayName': f"{os.environ.get('REPOSITORY_NAME')} {os.environ.get('VERSION')}",
+        'gameVersions': [11779],  # Minecraft 1.21.1
+        'gameVersionNames': ['Client', 'Server', 'NeoForge', '1.21.1'],
+        'releaseType': 'release',
+        'relations': {
+            'projects': []
         }
     }
 
-    with open("src/main/python/dependencies.curseforge.json", "r", encoding="utf-8") as dependencies_file:
-        metadata["relations"]["projects"] = json.load(dependencies_file)
+    with open('src/main/python/dependencies.curseforge.json', 'r', encoding='utf-8') as dependencies_file:
+        metadata['relations']['projects'] = json.load(dependencies_file)
 
-    for jar in Path().glob("*.jar"):
-        with jar.open("rb") as jar_file:
+    for jar in Path().glob('*.jar'):
+        with jar.open('rb') as jar_file:
             response = requests.post(
-                f"https://minecraft.curseforge.com/api/projects/{os.environ.get("CURSEFORGE_PROJECT_ID")}/upload-file",
+                f"https://minecraft.curseforge.com/api/projects/{os.environ.get('CURSEFORGE_PROJECT_ID')}/upload-file",
                 headers={
-                    "X-Api-Token": os.environ.get("CURSEFORGE_TOKEN"),
-                    "User-Agent": f"{os.environ.get("REPOSITORY")}/{os.environ.get("VERSION")}"
+                    'X-Api-Token': os.environ.get('CURSEFORGE_TOKEN'),
+                    'User-Agent': f"{os.environ.get('REPOSITORY')}/{os.environ.get('VERSION')}"
                 },
                 data={
-                    "metadata": json.dumps(metadata)
+                    'metadata': json.dumps(metadata)
                 },
                 files={
-                    "file": (
+                    'file': (
                         jar.name,
                         jar_file,
-                        "application/java-archive"
+                        'application/java-archive'
                     )
                 }
             )
@@ -45,5 +45,5 @@ def main():
             response.raise_for_status()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- src/main/python/release.modrinth.py
+++ src/main/python/release.modrinth.py
@@ -7,49 +7,50 @@ import requests
 
 def main():
     data = {
-        "name": f"{os.environ.get("REPOSITORY_NAME")} {os.environ.get("VERSION")}",
-        "version_number": os.environ.get("VERSION").removeprefix("v"),
-        "changelog": Path("CHANGELOG.md").read_text(encoding="utf-8"),
-        "dependencies": [],
-        "game_versions": ["1.21.1"],
-        "version_type": "release",
-        "loaders": ["neoforge"],
-        "featured": True,
-        "project_id": os.environ.get("MODRINTH_PROJECT_ID")
+        'name': f"{os.environ.get('REPOSITORY_NAME')} {os.environ.get('VERSION')}",
+        'version_number': os.environ.get('VERSION').removeprefix('v'),
+        'changelog': Path('CHANGELOG.md').read_text(encoding='utf-8'),
+        'dependencies': [],
+        'game_versions': ['1.21.1'],
+        'version_type': 'release',
+        'loaders': ['neoforge'],
+        'featured': True,
+        'project_id': os.environ.get('MODRINTH_PROJECT_ID')
     }
 
-    with open("src/main/python/dependencies.modrinth.json", "r", encoding="utf-8") as f:
-        data["dependencies"] = json.load(f)
+    with open('src/main/python/dependencies.modrinth.json', 'r', encoding='utf-8') as f:
+        data['dependencies'] = json.load(f)
 
     with ExitStack() as stack:
         files = {}
         file_parts = []
 
-        for jar in Path().glob("*.jar"):
-            file = stack.enter_context(jar.open("rb"))
+        for jar in Path().glob('*.jar'):
+            file = stack.enter_context(jar.open('rb'))
             file_part = jar.stem
             file_parts.append(file_part)
-            files[file_part] = (jar.name, file, "application/java-archive")
+            files[file_part] = (jar.name, file, 'application/java-archive')
 
         if not file_parts:
-            raise FileNotFoundError("No Jar files found.")
+            raise FileNotFoundError('No Jar files found.')
 
-        data["file_parts"] = file_parts
-        data["primary_file"] = file_parts[0]
+        data['file_parts'] = file_parts
+        data['primary_file'] = file_parts[0]
 
         response = requests.post(
-            "https://api.modrinth.com/v2/version",
+            'https://api.modrinth.com/v2/version',
             headers={
-                "Authorization": os.environ.get("MODRINTH_TOKEN"),
-                "User-Agent": f"{os.environ.get("REPOSITORY")}/{os.environ.get("VERSION")}"
+                'Authorization': os.environ.get('MODRINTH_TOKEN'),
+                'User-Agent': f"{os.environ.get('REPOSITORY')}/{os.environ.get('VERSION')}"
             },
             data={
-                "data": json.dumps(data)
+                'data': json.dumps(data)
             },
             files=files
         )
         print(response.text)
         response.raise_for_status()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
